6dc396ee57f651eedbaf3ea6f2fe08912c55a2aa
bishmie <bishmimalshi@gmail.com>
2026-03-31 14:54:30 +0530

    update yaml files

--- src/test/java/org/example/fileservice/FileServiceApplicationTests.java
+++ src/test/java/org/example/fileservice/FileServiceApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.fileservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.cloud.config.enabled=false")
 class FileServiceApplicationTests {
 
     @Test
